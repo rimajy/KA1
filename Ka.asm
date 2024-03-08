49c15e63212d864e6ba9dfa8a54e77f457b6a68f
rimajy <157909303+rimajy@users.noreply.github.com>
2024-03-08 12:23:30 +0200

    add aboba

--- Ka.asm
+++ Ka.asm
@@ -4,7 +4,19 @@
 .code
 main PROC  
 mov ah, 02h
-mov dl, '0'
+mov dl, 'a'
+int 21h
+mov ah, 02h
+mov dl, 'b'
+int 21h
+mov ah, 02h
+mov dl, 'o'
+int 21h
+mov ah, 02h
+mov dl, 'b'
+int 21h
+mov ah, 02h
+mov dl, 'a'
 int 21h
 main ENDP
 END main
