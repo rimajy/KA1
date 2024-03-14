1779f519b7f2dcb96d97b41ee48608a14b862475
rimajy <157909303+rimajy@users.noreply.github.com>
2024-03-14 18:51:36 +0200

    add input and output str

--- Ka.asm
+++ Ka.asm
@@ -1,22 +1,38 @@
-.model small
-.stack 100h
-.data
-.code
-main PROC  
-mov ah, 02h
-mov dl, 'a'
-int 21h
-mov ah, 02h
-mov dl, 'b'
-int 21h
-mov ah, 02h
-mov dl, 'o'
-int 21h
-mov ah, 02h
-mov dl, 'b'
-int 21h
-mov ah, 02h
-mov dl, 'a'
-int 21h
-main ENDP
-END main
+.MODEL SMALL
+.STACK 100H
+
+.DATA
+    MSG1 DB 'Enter a string: $'
+    MSG2 DB 0DH, 0AH, 'You entered: $'
+    BUFFER DB 100 DUP ('$')
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    ; Вивід повідомлення для введення рядка
+    LEA DX, MSG1
+    MOV AH, 09H
+    INT 21H
+
+    ; Зчитування рядка з клавіатури
+    MOV AH, 0AH
+    LEA DX, BUFFER
+    INT 21H
+
+    ; Вивід повідомлення з введеним рядком
+    LEA DX, MSG2
+    MOV AH, 09H
+    INT 21H
+
+    ; Вивід введеного рядка
+    LEA DX, BUFFER+2 ; Пропускаємо перші два байти, що містять довжину рядка
+    MOV AH, 09H
+    INT 21H
+
+    MOV AH, 4CH
+    INT 21H
+MAIN ENDP
+
+END MAIN
